8e4e39f37709927c81f8b63142449d070cb53e6e
Caitlyn <caitlynmiley.cm@gmail.com>
2021-06-03 12:55:58 -0500

    added some rows and columns to home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import React from 'react'
 import './App.css';
+import Home from "../src/components/Home"
 
 function App() {
   return (
     <div className="App">
        <h1>Test</h1> 
+       <Home />
     </div>
   );
 }
