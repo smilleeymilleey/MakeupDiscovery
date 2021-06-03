4e0ce23dd002eb1c6919d1f4188b5045c93e9b9e
Caitlyn <caitlynmiley.cm@gmail.com>
2021-06-03 12:51:38 -0500

    fixed file stucture and npm start the app

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+
+function Home() {
+    return (
+        <Container>
+            <Row>
+            <Col>1 of 3</Col>
+            <Col>2 of 3</Col>
+            <Col>3 of 3</Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Home
