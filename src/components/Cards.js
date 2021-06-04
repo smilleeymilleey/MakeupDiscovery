bcbdb9e94ac496813b43be6faa496422887c9ac5
Caitlyn <caitlynmiley.cm@gmail.com>
2021-06-04 10:43:28 -0500

    styled cards and added a .env

---
+++ src/components/Cards.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card'
+import CardDeck from 'react-bootstrap/CardDeck'
+import "../components/Cards.css"
+
+
+function Cards() {
+    return (
+        <div>
+          <CardDeck>
+            <Card className="cardContainer">
+                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                    This is a wider card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.
+                </Card.Text>
+                </Card.Body>
+               
+            </Card>
+            <Card className="">
+                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                    This card has supporting text below as a natural lead-in to additional
+                    content.{' '}
+                </Card.Text>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                    This is a wider card with supporting text below as a natural lead-in to
+                    additional content. This card has even longer content than the first to
+                    show that equal height action.
+                </Card.Text>
+                </Card.Body>
+            </Card>
+            </CardDeck>
+        </div>
+    )
+}
+
+export default Cards
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,15 +3,17 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
 import "../components/Home.css"
+import Cards from "./Cards"
 
 function Home() {
     return (
         <Container>
             <Row>
                 <Col className="searchColumn">
-                    <input className="searchbar"/>
+                    <input className="searchbar"/>    
                 </Col>
                 <Col className="productColumn">
+                   <Cards />
                 </Col>
             </Row>
         </Container>
