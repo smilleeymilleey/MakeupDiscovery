3094f8830a99e3731704458348a4d4f7ea427dd3
Caitlyn <caitlynmiley.cm@gmail.com>
2021-06-04 13:51:09 -0500

    saving my place before lunch. Adding get images fetch request

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react'
+import "../components/SearchBar.css"
 require('dotenv').config()
 
-// search bar api request for drop down menu
-
+//generates categories
 function SearchBar() {
     const [category, setCategory] = useState([])
+    const [image, setImage] = useState([])
     useEffect(() => {
         fetch(
             'https://api.thecatapi.com/v1/categories',
@@ -23,15 +24,42 @@ function SearchBar() {
             }
         })  
     });
+
+// get images 
+
+function getImages() {
+    useEffect(() => {
+        fetch('https://api.thecatapi.com/v1/images',
+            {   
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-api-key': 'Bearer a8e933a9-9c1e-42ce-886f-933c67cf09a0'
+                },
+            })
+            .then(response => response.json())
+            .then(response => {
+                console.log(response)
+                if (response.length > 0) {
+                    setImage(response)
+                }
+            })  
+        });
+        
+      getImages()
     return (
         <>
             {category.map(category => 
-                <p>
-                    {category.name}
-                </p>
+                <ul>
+                 <li>
+                    <button onClick={getImages(image)} className="buttons">
+                        {category.name}
+                    </button>
+                 </li>
+                </ul>
             )}
         </>
     )
 }
-
+}
 export default SearchBar
